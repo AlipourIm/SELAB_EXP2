0c80f5eb4c1e93b0b9542a7ec09cacd7b6715bcf
AlipourIm <iman.alip2001@gmail.com>
2023-03-19 23:32:58 +0330

    Add func in calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,4 +4,19 @@ public class Calculator {
     public int add (int a, int b) {
         return a + b;
     }
+
+    public double func(int a, String opt) throws Exception {
+        if (opt.equals("rvs")){
+            if (a == 0){
+                throw new Exception("cannot divide by zero");
+            }
+            return 1/a;
+        } else if (opt.equals("sqr")) {
+            if (a < 0){
+                throw new Exception("sqrt is not defined for negative numbers");
+            }
+            return Math.sqrt(a);
+        }
+        throw new Exception("undefined opt");
+    }
 }
